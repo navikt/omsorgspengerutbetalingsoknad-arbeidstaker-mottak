0109ec1add8503c6e4d61cc28257393807f66f40
Sander Opperud Odden <sander.opperud.odden@nav.no>
2021-11-10 10:31:02 +0100

    Oppdaterer dusseldorfKtorVersion, Kotlin JVM, Shadow, checkout og setup-java.
    Bruker JUnit5 for test.
    Samkjører Java versjon.
    Legger til kittinunf.fuel.
    Fjerner requestId fra metadata.
    Fjerner @KtorExperimentalAPI som er deprecated.
    
    * Closes #48
    * Closes #46
    * Closes #45

--- src/main/kotlin/no/nav/helse/mottak/v1/arbeidstaker/SoknadKafkaProducer.kt
+++ src/main/kotlin/no/nav/helse/mottak/v1/arbeidstaker/SoknadKafkaProducer.kt
@@ -73,7 +73,6 @@ private class SoknadOutgoingSerializer : Serializer<TopicEntry<JSONObject>> {
     override fun serialize(topic: String, data: TopicEntry<JSONObject>): ByteArray {
         val metadata = JSONObject()
             .put("correlationId", data.metadata.correlationId)
-            .put("requestId", data.metadata.requestId)
             .put("version", data.metadata.version)
 
         return JSONObject()
